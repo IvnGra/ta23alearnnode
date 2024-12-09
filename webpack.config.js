6a6e8c09c5338bf07a0343530c05585a1e8a43f9
ivan gra <gratsov.ivan@gmail.com>
2024-12-09 12:58:27 +0200

    09.12.24

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,79 @@
-import path from 'path';
-import HtmlWebpackPlugin from "html-webpack-plugin"
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
-
-export default {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(import.meta.dirname, 'dist'),
-  },
-  devServer: {
-    static: {
-      directory: path.join(import.meta.dirname, 'public'),
-    },
-    compress: true,
-    port: 9000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader","css-loader"],
+export default async () => {
+  let response = await fetch("https://rickandmortyapi.com/api/character");
+  let json = await response.json();
+  let characters = json.results;
+  let pages = [];
+  characters.forEach(character => {
+    let page = new HtmlWebpackPlugin({
+      template: './src/character.njk',
+      filename: 'character_' + character.id + '.html',
+      templateParameters: {
+        character
       },
+    });
+    pages.push(page);
+  });
+  return {
+    entry: "./src/index.js",
+    output: {
+      filename: "main.js",
+      path: path.resolve(import.meta.dirname, "dist"),
+    },
+    devServer: {
+      static: {
+        directory: path.join(import.meta.dirname, "public"),
+      },
+      compress: true,
+      port: 9000,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.scss$/i,
+          use: [
+            "style-loader",
+            "css-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                sassOptions: {
+                  quietDeps: true,
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.njk$/,
+          use: [
+            {
+              loader: "simple-nunjucks-loader",
+              options: {},
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.njk",
+        templateParameters: {
+          name: "Hannes",
+          characters, //characters: characters
+        },
+      }),
+      new HtmlWebpackPlugin({
+        filename: "about.html",
+        template: "./src/about.njk",
+      }),
+      ...pages
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-        template: './src/index.html'
-    })
-  ],
+  };
 };
