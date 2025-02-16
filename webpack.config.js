6af8e668d56a2aa479dc44a61aa1896382606b3e
ivan gra <gratsov.ivan@gmail.com>
2025-02-16 16:51:05 +0200

    RandM getpage

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,28 @@
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import { fileURLToPath } from "url";
-import fetch from "node-fetch";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 export default async () => {
-  
-  const fetchCharacters = async (page) => {
-    const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
-    const json = await response.json();
-    return json.results;
-  };
-
   let response = await fetch('https://rickandmortyapi.com/api/character?page=5');
-
-  
-  let characters = [...charactersPage7].slice(0, 5);
-
-  let pages = characters.map((character) => {
-    return new HtmlWebpackPlugin({
-      template: "./src/character.njk",
-      filename: `character_${character.id}.html`,
-      templateParameters: { character },
+  let json = await response.json();
+  let characters = json.results;
+  let pages = [];
+  characters.forEach(character => {
+    let page = new HtmlWebpackPlugin({
+      template: './src/character.njk',
+      filename: 'character_' + character.id + '.html', 
+      templateParameters: {
+        character
+      },
     });
+    pages.push(page);
   });
-
-  return {
-    entry: "./src/index.js",
+  
+    return {
+    entry: './src/index.js',
     output: {
-      filename: "main.js",
-      path: path.resolve(__dirname, "dist"),
+      filename: 'main.js',
+      path: path.resolve(import.meta.dirname, 'dist'),
     },
     devServer: {
       static: {
-        directory: path.join(__dirname, "public"),
+        directory: path.join(import.meta.dirname, 'public'),
       },
       compress: true,
       port: 9000,
@@ -47,43 +35,42 @@ export default async () => {
         },
         {
           test: /\.scss$/i,
-          use: [
-            "style-loader",
-            "css-loader",
-            {
-              loader: "sass-loader",
-              options: {
-                sassOptions: {
-                  quietDeps: true,
-                },
-              },
-            },
+          use: ["style-loader",
+             "css-loader", 
+             {
+             loader: "sass-loader",
+             options: {
+              sassOptions: {
+                quietDeps: true
+                }
+               }
+              } 
+            ],
+          },
+          {
+              test: /\.njk$/,
+              use: [
+                  {
+                      loader: 'simple-nunjucks-loader',
+                      options: {}
+                  }
+              ]
+          }
           ],
         },
-        {
-          test: /\.njk$/,
-          use: [
-            {
-              loader: "simple-nunjucks-loader",
-              options: {},
+        plugins: [
+          new HtmlWebpackPlugin({
+            template: './src/index.njk',
+            templateParameters: {
+              name: 'Ivan',
+              characters, //characters: characters
             },
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./src/index.njk",
-        templateParameters: {
-          name: "Ivan",
-          characters,
-        },
-      }),
-      new HtmlWebpackPlugin({
-        filename: "about.html",
-        template: "./src/about.njk",
-      }),
-      ...pages,
-    ],
-  };
-};
+          }),
+          new HtmlWebpackPlugin({
+            filename: 'about.html',
+            template: './src/about.njk',
+          }),
+          ...pages
+        ],
+      };
+    }
