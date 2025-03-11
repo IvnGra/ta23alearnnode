1818300dfce80d2a0a8ce71892631b595893233a
ivan gra <145213972+Dollaca@users.noreply.github.com>
2025-03-11 11:46:49 +0200

    rickAndMorty api numbers

---
+++ src/components/SimplePagination.vue
@@ -0,0 +1,9 @@
+<script setup>
+    defineProps(['info'])
+</script>
+<template>
+    <div class="buttons">
+        <button class="button is-primary" @click ="$emit('prev')" :disble = !info.prev>Prev</button>
+        <button class="button is-primary" @click ="$emit('next')" :disble = !info.next>Next</button>
+    </div>
+</template>
