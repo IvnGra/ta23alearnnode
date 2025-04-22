73be32e225643722f145fce5dfdcd8bd09b1f525
ivan gra <145213972+Dollaca@users.noreply.github.com>
2025-04-22 11:42:47 +0300

    add chat and polling example

--- bin/server.js
+++ bin/server.js
@@ -1,22 +1,28 @@
-import express  from 'express';
+import express from 'express';
 const app = express();
 const port = 3000;
 
-let messages = []
+let messages = [];
 
-app.use(express.urlencoded());
+app.use(express.json());
+
 app.use((req, res, next) => {
-
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'content-type');
+    next();
 });
-app.get('/', (req, res) => {
-  res.json(messages);
-})
 
-app.post('/', (req,res) => {
-    res.header('Access-Control-Allow-Origin')
-    res.json(req.body)
-})
+app.get('/', (req, res) => {
+    let date = req.query.date ?? null;
+    let filteredMessages = messages.filter(message => message.date > new Date(date));
+    res.json(filteredMessages);
+});
+
+app.post('/', (req, res) => {
+    messages.push({message: req.body.message, date: new Date()});
+    res.json(req.body);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`);
-}); 
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
